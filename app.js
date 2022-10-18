cc45dccdc8716bfdd1eb7c64fa07b5dd4d495619
m-francisca <mariafranciscaa@hotmail.com>
2022-10-17 22:09:02 -0300

    cambios en formulario y pagina principal

---
+++ app.js
@@ -0,0 +1,34 @@
+var nombre = document.getElementById('nombre');
+var password = document.getElementById('password');
+var domicilio = document.getElementById('domicilio');
+var error = document.getElementById('error');
+
+
+
+
+function enviarPedido(){
+    console.log('Enviando pedido...'); 
+
+     var mensajesError = [];
+
+    if(nombre.value === null || nombre.value === ''){
+    
+        alert('[ERROR] El campo nombre debe tener un valor...');
+
+    }
+
+    else if(password.value === null || password.value === ''){
+       
+        alert('[ERROR] El campo  contraseña debe tener un valor...');
+    }
+   else  if(domicilio.value === null || domicilio.value === ''){
+        
+         alert('[ERROR] El campo domicilio debe tener un valor...');
+     }
+   
+
+     error.innerHTML = mensajesError.join(', ');
+
+    return false;
+}
+
